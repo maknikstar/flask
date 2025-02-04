4eb5ba937b821f2a76a68b9f2363f6fde96c5f49
dentrunov <sgema@rambler.ru>
2025-02-04 20:54:47 +0300

    макар кое-что сделал

---
+++ app/forms.py
@@ -0,0 +1,33 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import ValidationError, DataRequired, Email, EqualTo
+import sqlalchemy as sa
+from app import db
+from app.models import User
+
+class LoginForm(FlaskForm):
+  username = StringField('Логин', validators=[DataRequired()])
+  password = PasswordField('Пароль', validators=[DataRequired()])
+  remember_me = BooleanField('Запомнить меня')
+  submit = SubmitField('Авторизация')
+
+class RegistrationForm(FlaskForm):
+  username = StringField('Логин', validators=[DataRequired()])
+  email = StringField('Электронная почта', validators=[DataRequired(), Email()])
+  password = PasswordField('Пароль', validators=[DataRequired()])
+  password2 = PasswordField(
+    'Повторите пароль ', validators=[DataRequired(), EqualTo('Пароль')])
+  submit = SubmitField('Регистрация')
+
+  def validate_username(self, username):
+    user = db.session.scalar(sa.select(User).where(
+      User.username == username.data))
+    if user is not None:
+      raise ValidationError('используйте другое имя пользователя')
+
+  def validate_email(self, email):
+    user = db.session.scalar(sa.select(User).where(
+      User.email == email.data))
+    if user is not None:
+      raise ValidationError('используйте другую электронную почту')
+
--- app/models.py
+++ app/models.py
@@ -3,7 +3,7 @@ import sqlalchemy as sa
 import sqlalchemy.orm as so
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
-
+from werkzeug.security import generate_password_hash, check_password_hash
 from app import db, login
 
 
@@ -12,7 +12,14 @@ class User(UserMixin, db.Model):
     username: so.Mapped[str] = so.mapped_column(sa.String(64), index=True, unique=True)
     email: so.Mapped[str] = so.mapped_column(sa.String(120), index=True, unique=True)
     password_hash: so.Mapped[Optional[str]] = so.mapped_column(sa.String(256))
-  
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+
     def __repr__(self):
         return '<Пользователь {}>'.format(self.username)
   
@@ -25,3 +32,4 @@ class User(UserMixin, db.Model):
 @login.user_loader
 def load_user(id):
   return db.session.get(User, int(id))
+
