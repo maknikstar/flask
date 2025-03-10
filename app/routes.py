549c47c7a54f102e38a5738f011a1d49e1463d8f
dentrunov <sgema@rambler.ru>
2025-03-10 21:23:27 +0300

    выборка данных

--- app/routes.py
+++ app/routes.py
@@ -106,8 +106,12 @@ def edit_data():
 @app.route("/dashboard")
 # @login_required
 def dashboard():
-    # students = db.session.scalars(sa.select(Student).join(Classes, Classes.id==Student.class_id))
-    students = Student.query.join(Classes, Student.class_id==Classes.id)
+    students = db.session.scalars(sa.select(
+        Student.name,
+        Student.surname,
+        Classes.class_name
+        ).join(Classes, Classes.id==Student.class_id))
+    # students = Student.query.join(Classes, Student.class_id==Classes.id)
     for st in students:
         print(st.__dict__)
     return render_template('dashboard.html', title='Панель управления', students=students)
